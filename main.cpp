a21d24beec7d382c00de5d62b3d5282741efb9be
Radek <radek3301@linux.pl>
2018-12-06 16:20:02 +0100

    added variable frequency

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 int main() {
 	wiringPiSetupGpio();
-	int pin;
+	int pin, frequency;
 
 	system("clear");
 	cout << "GPIO pin: ";
 	cin >> pin;
+	cout << endl << "Frequency: ";
+	cin >> frequency;
 	cout << endl;
 
 	pinMode(pin, OUTPUT);
@@ -19,17 +21,19 @@ int main() {
 	for(int i = 1; i <= 10; i++) {
 		cout << "--------------------------------------------------" << endl;
 		digitalWrite(pin, HIGH);
-		cout << "> Pin " << pin << endl;
-		cout << "> Status HIGH,	" << i << "/10" << endl;
+		cout << "> Pin: " << pin << endl;
+		cout << "> Frequency: " << frequency << endl;
+		cout << "> Status: HIGH,	" << i << "/10" << endl;
 		cout << "--------------------------------------------------" << endl;
-		delay(1000);
+		delay(frequency);
 		system("clear");
 		cout << "--------------------------------------------------" << endl;
 		digitalWrite(pin, LOW);
 		cout << "> Pin " << pin << endl;
-                cout << "> Status LOW,	" << i << "/10" << endl;
+		cout << "> Frequency: " << frequency << endl;
+        cout << "> Status LOW,	" << i << "/10" << endl;
 		cout << "--------------------------------------------------" << endl;
-		delay(1000);
+		delay(frequency);
 		system("clear");
 	}
 
