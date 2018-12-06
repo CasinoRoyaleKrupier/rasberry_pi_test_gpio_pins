f62d59fb415b9987a1800e9fd8cfa9b2ababa124
Radek <radek3301@linux.pl>
2018-12-06 10:35:38 -0500

    v0.2 added quantity

--- main.cpp
+++ main.cpp
@@ -6,32 +6,31 @@ using namespace std;
 
 int main() {
 	wiringPiSetupGpio();
-	int pin, frequency;
+	int pin, frequency, quantity;
 
 	system("clear");
-	cout << "GPIO pin: ";
-	cin >> pin;
-	cout << endl << "Frequency: ";
-	cin >> frequency;
+	cout << "GPIO pin: ";	cin >> pin;
+	cout << "Frequency: ";	cin >> frequency;
+	cout << "Quantity: ";	cin >> quantity;
 	cout << endl;
 
 	pinMode(pin, OUTPUT);
 	system("clear");
 
-	for(int i = 1; i <= 10; i++) {
+	for(int i = 1; i <= quantity; i++) {
 		cout << "--------------------------------------------------" << endl;
 		digitalWrite(pin, HIGH);
 		cout << "> Pin: " << pin << endl;
 		cout << "> Frequency: " << frequency << endl;
-		cout << "> Status: HIGH,	" << i << "/10" << endl;
+		cout << "> Status: HIGH,	" << i << "/" << quantity << endl;
 		cout << "--------------------------------------------------" << endl;
 		delay(frequency);
 		system("clear");
 		cout << "--------------------------------------------------" << endl;
 		digitalWrite(pin, LOW);
-		cout << "> Pin " << pin << endl;
+		cout << "> Pin: " << pin << endl;
 		cout << "> Frequency: " << frequency << endl;
-        cout << "> Status LOW,	" << i << "/10" << endl;
+        	cout << "> Status: LOW,	" << i << "/" << quantity << endl;
 		cout << "--------------------------------------------------" << endl;
 		delay(frequency);
 		system("clear");
